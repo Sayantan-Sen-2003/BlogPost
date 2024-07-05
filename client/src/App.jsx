b75bd96022eb1b830842b7c8c38211dac2a665a3
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-07-06 03:10:04 +0530

    added routes

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
