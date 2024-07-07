a9d323dbaaaafa3fc7104ed12cf8f4ff69a487bd
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-07-07 18:13:25 +0530

    made dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import SignUp from './pages/SignUp'
 import Footer from './components/Footer';
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute';
+
 
 export default function App() {
   return (
@@ -17,8 +19,10 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn/>} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       
