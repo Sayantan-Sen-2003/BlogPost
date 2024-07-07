8c49fb57be1fafa086e278fb7cc4a229c02a5842
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-07-08 00:52:59 +0530

    updated ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
-          <Route path='/update-post/:postId' element={<UpdatePost />} />
+          {/* <Route path='/update-post/:postId' element={<UpdatePost />} /> */}
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
