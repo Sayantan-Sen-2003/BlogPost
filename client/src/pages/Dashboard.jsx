ea70c1bf7c79e64ce81787175a559b3eb86d8789
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-07-08 18:10:35 +0530

    added show users

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
-// import DashUsers from '../components/DashUsers';
+import DashUsers from '../components/DashUsers';
 // import DashComments from '../components/DashComments';
 // import DashboardComp from '../components/DashboardComp';
 
