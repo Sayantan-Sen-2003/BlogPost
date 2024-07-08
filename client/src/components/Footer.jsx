eb5c7fb0ac9b7d19c510e4b27ff708ae12fbdd94
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-07-08 20:21:30 +0530

    modified ui

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+//WIP
+
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -102,6 +102,7 @@ export default function CreatePost() {
               setFormData({ ...formData, category: e.target.value })
             }
           >
+            //WIP
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>JavaScript</option>
             <option value='reactjs'>React.js</option>
