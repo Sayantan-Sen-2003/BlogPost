29622a5addfa7b52d43400acb57b957895b0e40b
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-07-08 18:01:37 +0530

    added update Post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 
 export default function App() {
@@ -27,7 +28,7 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
-          {/* <Route path='/update-post/:postId' element={<UpdatePost />} /> */}
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
