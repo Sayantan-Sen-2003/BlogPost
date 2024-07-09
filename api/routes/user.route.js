a38b1850439933670c7a9ff1a0abcf9029daccd0
Sayantan Sen <sayantansen530@gmail.com>
2024-07-09 10:16:03 +0530

    get user route to check how many users are there

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUsers, signout, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -9,4 +9,5 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 export default router;
