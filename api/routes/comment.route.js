b08f6dc4f6e233946f2681ebcc31e407edc84202
Sayantan Sen <sayantansen530@gmail.com>
2024-07-09 11:25:19 +0530

    getPostComment-->for a particular post ;no admin access needed ; getcomments (currently added its route and functionality) is for an admin to check all comments in specified time

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, deleteComment, editComment, getPostComments, likeComment } from '../controllers/comment.controller.js';
+import { createComment, deleteComment, editComment, getcomments, getPostComments, likeComment } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
@@ -10,5 +10,5 @@ router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
-
+router.get('/getcomments', verifyToken, getcomments);
 export default router;
